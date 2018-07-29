ff76a1711aa5ddc33bfc1b4668a16f3c8eb382be
Selezneva Anna <selezneva_anna@icloud.com>
2018-07-29 18:47:38 +0500

    add mirror servlet to context

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,19 +4,22 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlets.AllRequestsServlet;
-
-import javax.servlet.Servlet;
+import servlets.MirrorRequestServlet;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+        MirrorRequestServlet mirrorRequestServlet = new MirrorRequestServlet();
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder((Servlet) allRequestsServlet), "/*");
+
+        context.addServlet(new ServletHolder(mirrorRequestServlet), "/mirror");
+        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
 
         Server server = new Server(8080);
         server.setHandler(context);
 
         server.start();
+        java.util.logging.Logger.getGlobal().info("Server started");
         server.join();
     }
 }
